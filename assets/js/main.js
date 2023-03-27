da28587cf4fce36ff801d7031121a94dcdf9ced7
Giorgia Crosilla <giorgia.crosilla@studio.unibo.it>
2023-03-27 23:40:37 +0200

    zoomed image

--- assets/js/main.js
+++ assets/js/main.js
@@ -228,33 +228,16 @@
     }
   });
 
-  /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
-
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 20
-      }
-    }
+  function zoomImage() {
+    var overlay = document.getElementById("overlay");
+    var zoomed = document.getElementById("zoomed");
+    overlay.style.display = "block";
+    zoomed.style.display = "block";
+  }
+  
+  document.getElementById("overlay").addEventListener("click", function(){
+    this.style.display = "none";
+    document.getElementById("zoomed").style.display = "none";
   });
 
   /**
